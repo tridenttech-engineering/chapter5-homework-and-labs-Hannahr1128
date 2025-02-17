47cce4c19d1e0203895b32b87edaf3caca29bb5c
Hannahr1128 <hannahr0705@gmail.com>
2025-02-17 16:11:50 +0000

    Submission for Hw5-12

--- .ccls-cache/@home@runner@workspace/hw5-12.cpp
+++ .ccls-cache/@home@runner@workspace/hw5-12.cpp
@@ -7,7 +7,23 @@ using namespace std;
 
 int main()
 {
-	
+	//declare variables
+	double price_1 = 0.0;
+	double price_2 = 0.0;
+
+	//enter input items
+	cout << "Enter price of the first item: ";
+	cin >> price_1;
+	cout << "Enter price of the second item: ";
+	cin >> price_2;
+
+	//calculate total owed
+	double minPrice = min(price_1, price_2);
+	double discount = minPrice / 2.0;
+	double totalCost = price_1 + price_2 - discount;
+
+	//display total owed
+	cout << "Total cost: $" << totalCost << endl;
 	
 	return 0;
 }	//end of main function
